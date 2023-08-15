75fc23b4de4b07ca16e54dfcf0c4347f8e70a89a
julia de lima <lima.machado@alunoescolassociais.org.br>
2023-08-15 08:10:37 -0300

    mudei nome do codigo para o index.html

--- cadastro.js
+++ cadastro.js
@@ -1,22 +1,22 @@
-function verificar() {
+function verificar(){
+    let senha = document.getElementById('senha').value;
+    let confirmar = document.getElementById('confirmar').value;
+    let email = document.getElementById('email').value;
+    let cpf = document.getElementById('cpf').value;
+    let cep = document.getElementById('cep').value;
+    let nome = document.getElementById('nome').value;
+    let endereco = document.getElementById('endereco').value;
+    let bairro = document.getElementById('bairro').value;
+    let cidade = document.getElementById('cidade').value;
+    let estado = document.getElementById('estado').value;
+    let data = document.getElementById('data').value
 
-    let Nome = document.getElementById('Nome').value;
-   let DatadeNascimento = document.getElementById('dtnasc').value;
-   let CPF = document.getElementById('CPF').value;
-   let Email = document.getElementById('email').value;
-    let telefone = document.getElementById('telefone').value;
-    let CEP = document.getElementById('CEP').value;
-   let Endereço = document.getElementById('Endereco').value;
-   let Numero = document.getElementById('Numero').value;
-   let Complemento = document.getElementById('Complemento').value;
-   let Bairro = document.getElementById('Bairro').value;
-   let cidade = document.getElementById('cidade').value;
-   let Estado = document.getElementById('Estado').value;
-   let ref = document.getElementById('ref').value;
-   let confirmSenha = document.getElementById('confirmsenha').value;
-   let senha = document.getElementById('Senha').value;
-   if (!Nome || !DatadeNascimento || !CPF || !Email || !telefone || !CEP || !Endereço || !Numero || !Complemento || !Bairro ||  || !cidade !Estado || !ref || !confirmSenha || !senha) {
    
-    alert("Campo de preenchimento obrigatório");
-   }
-       }
+
+if( !data || !confirmar || !cep ||!nome || !cpf || !senha ||!email || !endereco || !bairro || !cidade || !estado ){
+   alert("porfavor preencher todos os campos");
+}
+else{
+   alert("Campos preenchidos com sucesso!");
+}
+}
